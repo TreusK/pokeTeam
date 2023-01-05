a19ed40151ce8e43277970c87bdf30f3735c90cd
TreusK <flutri@protonmail.com>
2023-01-04 22:46:47 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css';
+
+function App() {
+   return (
+       <div className='bg-red-400'>
+           <h1>Hi App</h1>
+       </div>
+   )
+}
+
+export default App
