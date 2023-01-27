305b88abfff17579574d376dc015617a52f06834
anon1911 <86053169+anon1911@users.noreply.github.com>
2023-01-28 12:55:25 +1300

    refactor: apply autoformatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,60 @@
-import { useState, useEffect } from 'react'
-import {Routes, Route} from 'react-router-dom';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
 //Components
-import Header from './components/Header';
-import Navbar from './components/Navbar';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 //Pages
-import Home from './pages/Home';
-import Team from './pages/Team';
-import About from './pages/About';
-import './App.css';
+import Home from "./pages/Home";
+import Team from "./pages/Team";
+import About from "./pages/About";
 //Extra
 
-
 function App() {
-    const [teams, setTeams] = useState([]);
-    const [pokeNames, setPokeNames] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [teams, setTeams] = useState([]);
+  const [pokeNames, setPokeNames] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    //Fetch the names of pokemon to populate search bar
-    useEffect(() => {
-        async function getPokes() {
-            try {
-                const response = await axios.get('https://pokeapi.co/api/v2/pokemon-species/?limit=20000');
-                setPokeNames(response.data.results);
-            } catch(err) {
-                console.log(err.message)
-            } finally {
-                setLoading(false);
-            }
-        }
-        getPokes();
-    }, []); 
-
-    function handleSaveTeam(currentTeam, teamIsNotFull) {
-        if(!teamIsNotFull(currentTeam)) {
-            setTeams(oldTeams => [...oldTeams, currentTeam])
-            console.log('Saved!')
-        }
+  //Fetch the names of pokemon to populate search bar
+  useEffect(() => {
+    async function getPokes() {
+      try {
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon-species/?limit=20000"
+        );
+        setPokeNames(response.data.results);
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        setLoading(false);
+      }
     }
+    getPokes();
+  }, []);
 
+  function handleSaveTeam(currentTeam, teamIsNotFull) {
+    if (!teamIsNotFull(currentTeam)) {
+      setTeams((oldTeams) => [...oldTeams, currentTeam]);
+      console.log("Saved!");
+    }
+  }
 
-    return (
-        <div className='h-screen flex flex-col'>
-            <Header />
-            <Navbar />
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/team' element={<Team pokeNames={pokeNames} handleSaveTeam={handleSaveTeam}/>} />
-                <Route path='/about' element={<About />} />
-            </Routes>
-        </div>
-    )
+  return (
+    <div className="h-screen flex flex-col">
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/team"
+          element={
+            <Team pokeNames={pokeNames} handleSaveTeam={handleSaveTeam} />
+          }
+        />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,106 +1,121 @@
 //import './Team.css';
-import {useState} from 'react';
-import {nanoid} from 'nanoid';
-import axios from 'axios';
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import axios from "axios";
 
-import Card from '../components/Card';
-import Input from '../components/Input';
-
+import Card from "../components/Card";
+import Input from "../components/Input";
 
 const arr = [0, 1, 2, 3, 4, 5];
 
-function Team({pokeNames, handleSaveTeam}) {
-    const [currentTeam, setCurrentTeam] = useState([
-        {cardIndex:0, canBeReplaced: true},
-        {cardIndex:1, canBeReplaced: true},
-        {cardIndex:2, canBeReplaced: true},
-        {cardIndex:3, canBeReplaced: true},
-        {cardIndex:4, canBeReplaced: true},
-        {cardIndex:5, canBeReplaced: true},
-    ]);
+function Team({ pokeNames, handleSaveTeam }) {
+  const [currentTeam, setCurrentTeam] = useState([
+    { cardIndex: 0, canBeReplaced: true },
+    { cardIndex: 1, canBeReplaced: true },
+    { cardIndex: 2, canBeReplaced: true },
+    { cardIndex: 3, canBeReplaced: true },
+    { cardIndex: 4, canBeReplaced: true },
+    { cardIndex: 5, canBeReplaced: true },
+  ]);
 
-
-    async function handleAddPoke(value) {
-        if(inputIsValid(value) && !alreadyInTeam(currentTeam, value) && teamIsNotFull(currentTeam)) {
-            try {
-                const pokeInfo = await axios.get(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`);
-                let poke = makePokeObject(pokeInfo.data);
-                console.log(poke)
-                setCurrentTeam(oldTeam => {
-                    let replaceableIndex = findFirstReplaceable(oldTeam);
-                    poke.cardIndex = replaceableIndex;
-                    let copy = [...oldTeam];
-                    copy[replaceableIndex] = poke;
-                    return copy;
-                });
-            } catch(err) {
-                console.log(err.message)
-            } finally {
-                console.log(value)
-            }
-        }
+  async function handleAddPoke(value) {
+    if (
+      inputIsValid(value) &&
+      !alreadyInTeam(currentTeam, value) &&
+      teamIsNotFull(currentTeam)
+    ) {
+      try {
+        const pokeInfo = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`
+        );
+        let poke = makePokeObject(pokeInfo.data);
+        console.log(poke);
+        setCurrentTeam((oldTeam) => {
+          let replaceableIndex = findFirstReplaceable(oldTeam);
+          poke.cardIndex = replaceableIndex;
+          let copy = [...oldTeam];
+          copy[replaceableIndex] = poke;
+          return copy;
+        });
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        console.log(value);
+      }
     }
+  }
 
-    //Event handlers
-    function handleDeletePoke(poke) {
-        setCurrentTeam(oldTeam => {
-            let replaceableIndex = poke.cardIndex;
-            let copy = [...oldTeam];
-            copy[replaceableIndex] = {cardIndex:poke.cardIndex, canBeReplaced: true};
-            return copy;
-        })
-    }
+  //Event handlers
+  function handleDeletePoke(poke) {
+    setCurrentTeam((oldTeam) => {
+      let replaceableIndex = poke.cardIndex;
+      let copy = [...oldTeam];
+      copy[replaceableIndex] = {
+        cardIndex: poke.cardIndex,
+        canBeReplaced: true,
+      };
+      return copy;
+    });
+  }
 
-    //Helper functions
-    function alreadyInTeam(teamArr, name) {
-        return teamArr.some(elem => elem.name == name)
-    }
+  //Helper functions
+  function alreadyInTeam(teamArr, name) {
+    return teamArr.some((elem) => elem.name == name);
+  }
 
-    function teamIsNotFull(teamArr) {
-        return teamArr.some(elem => elem.canBeReplaced == true);
-    }
+  function teamIsNotFull(teamArr) {
+    return teamArr.some((elem) => elem.canBeReplaced == true);
+  }
 
-    function inputIsValid(input) {
-        return (input && input[0] !== ' ');
-    }
+  function inputIsValid(input) {
+    return input && input[0] !== " ";
+  }
 
-    function getTypes(arrOfTypes) {
-        return arrOfTypes.map(typeObj => typeObj.type.name);
-    }
+  function getTypes(arrOfTypes) {
+    return arrOfTypes.map((typeObj) => typeObj.type.name);
+  }
 
-    function makePokeObject(data) {
-        return {
-            id: data.id, 
-            name: data.name,    
-            sprite: data.sprites.front_default, 
-            types: getTypes(data.types),
-            baseStats: data.stats,
-            canBeReplaced: false,
-        }
-    }
+  function makePokeObject(data) {
+    return {
+      id: data.id,
+      name: data.name,
+      sprite: data.sprites.front_default,
+      types: getTypes(data.types),
+      baseStats: data.stats,
+      canBeReplaced: false,
+    };
+  }
 
-    function findFirstReplaceable(oldTeam) {
-        return oldTeam.findIndex(elem => elem.canBeReplaced === true);
-    }
+  function findFirstReplaceable(oldTeam) {
+    return oldTeam.findIndex((elem) => elem.canBeReplaced === true);
+  }
 
-   return (
-       <div>
-            <Input pokeNames={pokeNames} handleAddPoke={handleAddPoke}/>
-            <div className="bg-gray-400 p-4 w-full mx-auto grid grid-cols-2 justify-items-center gap-5 xs:grid-cols-3 sm:w-4/5 lg:grid-cols-6 lg:w-fit">
-                {arr.map(elem => {
-                    return !currentTeam[elem].canBeReplaced 
-                        ? <Card key={nanoid()} poke={currentTeam[elem]} handleDeletePoke={handleDeletePoke}/>
-                        : <Card key={nanoid()}/>
-                })}
-            </div>   
-            <div className='text-center my-2'>
-                <button className='bg-blue-200 ml-2 rounded p-2 px-6 text-gray-500 hover:bg-blue-300' 
-                        onClick={() => handleSaveTeam(currentTeam, teamIsNotFull)}>Save team</button>
-            </div> 
-            
-        </div>
-   )
+  return (
+    <div>
+      <Input pokeNames={pokeNames} handleAddPoke={handleAddPoke} />
+      <div className="bg-gray-400 p-4 w-full mx-auto grid grid-cols-2 justify-items-center gap-5 xs:grid-cols-3 sm:w-4/5 lg:grid-cols-6 lg:w-fit">
+        {arr.map((elem) => {
+          return !currentTeam[elem].canBeReplaced ? (
+            <Card
+              key={nanoid()}
+              poke={currentTeam[elem]}
+              handleDeletePoke={handleDeletePoke}
+            />
+          ) : (
+            <Card key={nanoid()} />
+          );
+        })}
+      </div>
+      <div className="text-center my-2">
+        <button
+          className="bg-blue-200 ml-2 rounded p-2 px-6 text-gray-500 hover:bg-blue-300"
+          onClick={() => handleSaveTeam(currentTeam, teamIsNotFull)}
+        >
+          Save team
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Team
-
+export default Team;
